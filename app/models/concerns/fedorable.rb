adb8267aec3ff46db9c6114e8f6c1f1b68185223
Andrew Curley <andrew.curley@gmail.com>
2013-06-12 00:33:18 -0400

    Refactor some code and provide ability to write datastreams

--- app/models/concerns/fedorable.rb
+++ app/models/concerns/fedorable.rb
@@ -6,15 +6,47 @@ module Concerns::Fedorable
   included do
     has_one :fedora_attribute, as: :fedorable, class_name: 'FedoraRails::FedoraAttributes'
 
-    delegate :pid, to: :fedora_attribute, allow_nil: true
-    delegate :object_exists?, to: :fedora_attribute
+    delegate :pid, :object_exists?,
+      to: :fedora_attribute, allow_nil: true
 
-    after_save :check_fedora_attribute_object
+    after_save :ensure_fedora_attribute_object, :create_or_update_fedora_object
+    
+    @@repo = Rubydora.connect :url => 'http://localhost:8983/fedora', :user => 'fedoraAdmin', :password => 'fedoraAdmin'
+
+    def object_exists?
+      begin
+        @@repo.find(self.pid)
+        true
+      rescue
+        false
+      end
+    end
 
     private
 
-    def check_fedora_attribute_object
-      FedoraRails::FedoraAttributes.create(fedorable_id: self.id, fedorable_type: self.class.name) unless fedora_attribute
+    def ensure_fedora_attribute_object
+      if fedora_attribute.nil?
+        self.fedora_attribute = FedoraRails::FedoraAttributes.create(fedorable_id: self.id, fedorable_type: self.class.name)
+      end
+    end
+
+    def create_or_update_fedora_object
+      obj = @@repo.find_or_initialize(self.pid)
+      obj.save if obj.new?
+      create_or_update_metadata_datastream(obj)
+    end
+
+    def create_or_update_metadata_datastream(obj)
+      ds = obj.datastreams['preservationMetadata']
+      if ds.new?
+        ds.dsLabel = 'FedoraRails XML Serialization'
+        ds.mimeType = 'text/xml'
+        ds.controlGroup = 'M'
+        ds.dsState = "A"
+        ds.versionable = true
+      end
+      ds.content = self.to_xml
+      ds.save
     end
   end
 end
--- app/models/fedora_rails/fedora_attributes.rb
+++ app/models/fedora_rails/fedora_attributes.rb
@@ -5,32 +5,12 @@ module FedoraRails
     attr_accessible :pid, :fedorable_id, :fedorable_type
 
     belongs_to :fedorable, polymorphic: true
-
     before_save :assign_pid
-    after_save :create_or_update_fedora_object
-    
+
     @@repo = Rubydora.connect :url => 'http://localhost:8983/fedora', :user => 'fedoraAdmin', :password => 'fedoraAdmin'
 
     def assign_pid
       self.pid = @@repo.mint if self.pid.nil?
-    end
-
-    def create_or_update_fedora_object
-      obj = @@repo.find_or_initialize(self.pid)
-      obj.save if obj.new?
-    end
-
-    def create_or_update_metadata_datastream
-
-    end
-
-    def object_exists?
-      begin
-        @@repo.find(self.pid)
-        true
-      rescue
-        false
-      end
     end
   end
 end
