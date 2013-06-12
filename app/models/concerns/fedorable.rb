1b1a8b2720bac12422adff58d32d6f2ee4911f39
Andrew Curley <andrew.curley@gmail.com>
2013-06-12 10:41:51 -0400

    Refactor Fedorable Concern

--- app/models/concerns/fedorable.rb
+++ app/models/concerns/fedorable.rb
@@ -9,7 +9,7 @@ module Concerns::Fedorable
     delegate :pid, :object_exists?,
       to: :fedora_attribute, allow_nil: true
 
-    after_save :ensure_fedora_attribute_object, :create_or_update_fedora_object
+    after_save :synchronize_to_fedora
     
     @@repo = Rubydora.connect :url => 'http://localhost:8983/fedora', :user => 'fedoraAdmin', :password => 'fedoraAdmin'
 
@@ -24,29 +24,21 @@ module Concerns::Fedorable
 
     private
 
-    def ensure_fedora_attribute_object
+    def synchronize_to_fedora
+      # Check to see that a FedoraAttribute object exists
       if fedora_attribute.nil?
         self.fedora_attribute = FedoraRails::FedoraAttributes.create(fedorable_id: self.id, fedorable_type: self.class.name)
       end
-    end
 
-    def create_or_update_fedora_object
+      # Create object if it doesn't exist, otherwise read into memory
       obj = @@repo.find_or_initialize(self.pid)
       obj.save if obj.new?
-      create_or_update_metadata_datastream(obj)
-    end
+      
+      # Create datasream for serialized XML if it doesn't exist, otherwise just update XML contnet
+      # Rubydora params for creating a datastream:
+      # {:controlGroup => :dsControlGroup, :dsLocation => :dsLocation, :altIDs => nil, :dsLabel => :dsLabel, :versionable => :dsVersionable, :dsState => :dsState, :formatURI => :dsFormatURI, :checksumType => :dsChecksumType, :checksum => :dsChecksum, :mimeType => :dsMIME, :logMessage => nil, :ignoreContent => nil, :lastModifiedDate => nil, :content => nil, :asOfDateTime => nil}
 
-    def create_or_update_metadata_datastream(obj)
-      ds = obj.datastreams['preservationMetadata']
-      if ds.new?
-        ds.dsLabel = 'FedoraRails XML Serialization'
-        ds.mimeType = 'text/xml'
-        ds.controlGroup = 'M'
-        ds.dsState = "A"
-        ds.versionable = true
-      end
-      ds.content = self.to_xml
-      ds.save
+      Rubydora::Datastream.new(obj, 'preservationMetadata', {dsLabel: 'FedoraRails XML Serialization', mimeType: 'text/xml',controlGroup: 'M', dsState: 'A', versionable: true, content: self.to_xml}).save
     end
   end
 end
